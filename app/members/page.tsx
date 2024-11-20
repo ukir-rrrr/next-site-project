877b955957d7d9ddb82178895d34fce90ec9a51f
ukir-rrrr <aorikuaoriku@icloud.com>
2024-11-21 01:28:12 +0900

    MemberpageCreate

---
+++ app/members/page.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+const data = {
+  contents: [
+    {
+      id: "1",
+      image: {
+        url: "/img-member1.jpg",
+        width: 240,
+        height: 240,
+      },
+      name: "デイビッド・チャン",
+      position: "CEO",
+      profile: "グローバルテクノロジー企業での豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部として勤務し、新市場進出や収益成長に成功",
+    },
+    {
+      id: "2",
+      image: {
+        url: "/img-member2.jpg",
+        width: 240,
+        height: 240,
+      },
+      name: "エミリー・サンダース",
+      position: "COO",
+      profile: "グローバルテクノロジー企業での豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部として勤務し、新市場進出や収益成長に成功",
+    },
+    {
+      id: "3",
+      image: {
+        url: "/img-member3.jpg",
+        width: 240,
+        height: 240,
+      },
+      name: "ジョン・スミス",
+      position: "CTO",
+      profile: "グローバルテクノロジー企業での豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部として勤務し、新市場進出や収益成長に成功",
+    },
+  ]
+}
+
+export default function Page() {
+  return (
+    <div className={styles.container}>
+      {data.contents.length === 0 ? (
+        <p className={styles.empty}>メンバーが登録されていません。</p>
+      ) : (
+        <ul>
+          {data.contents.map((member) => (
+            <li key={member.id} className={styles.list}>
+              <Image
+                src={member.image.url}
+                alt=""
+                width={member.image.width}
+                height={member.image.height}
+                className={styles.image}
+              />
+              <dl>
+                <dt className={styles.name}>{member.name}</dt>
+                <dt className={styles.position}>{member.position}</dt>
+                <dt className={styles.profile}>{member.profile}</dt>
+              </dl>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
