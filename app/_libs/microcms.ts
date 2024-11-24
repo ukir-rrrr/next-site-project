2a29c0958676d5ed2755156499938703935b4082
ukir-rrrr <aorikuaoriku@icloud.com>
2024-11-24 17:48:12 +0900

    API Communication Settings

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,6 +1,22 @@
+import { createClient  } from "microcms-js-sdk";
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSListContent,
+} from "microcms-js-sdk";
+
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
+
+
 export type Category = {
   name: string;
 }
+
 
 export type News = {
   id: string;
@@ -10,4 +26,25 @@ export type News = {
   };
   publishedAt: string;
   createdAt: string;
+};
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICRO_API_KEY is required");
+}
+
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+export const getMemberList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
+  return listData
 };
--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,45 +1,10 @@
 import Image from "next/image";
+import { getMemberList } from "@/app/_libs/microcms";
 import styles from "./page.module.css";
+import { MEMBER_LIST_LIMIT } from "../_constants";
 
-const data = {
-  contents: [
-    {
-      id: "1",
-      image: {
-        url: "/img-member1.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "デイビッド・チャン",
-      position: "CEO",
-      profile: "グローバルテクノロジー企業での豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部として勤務し、新市場進出や収益成長に成功",
-    },
-    {
-      id: "2",
-      image: {
-        url: "/img-member2.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "エミリー・サンダース",
-      position: "COO",
-      profile: "グローバルテクノロジー企業での豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部として勤務し、新市場進出や収益成長に成功",
-    },
-    {
-      id: "3",
-      image: {
-        url: "/img-member3.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "ジョン・スミス",
-      position: "CTO",
-      profile: "グローバルテクノロジー企業での豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部として勤務し、新市場進出や収益成長に成功",
-    },
-  ]
-}
-
-export default function Page() {
+export default async function Page() {
+  const data = await getMemberList({ limit: MEMBER_LIST_LIMIT });
   return (
     <div className={styles.container}>
       {data.contents.length === 0 ? (
